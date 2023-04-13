777cf402398e3a0ac451ea0d8e38cf2e60d47c24
gururani-abhishek <gururani.abhishek@gmail.com>
2023-04-13 18:55:01 +0530

    first commit : set up WelcomeFragment

---
+++ app/src/main/java/com/example/secretmessage/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.secretmessage.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.secretmessage.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
